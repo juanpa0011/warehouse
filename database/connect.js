e07122b8ffb4c98b3a6bab5a17631844f7fbc9da
juanpa0011 <juanpablo1996epifani@gmail.com>
2021-10-29 21:05:46 -0300

    Readme, Env, Database Changes

--- database/connect.js
+++ database/connect.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql');
 const path = 'mysql://root@localhost:3000'; // As long as it is LOCALHOST:3000, it will search there
 
-const DB = mysql.createConnection({
-    host: 'localhost', // process.env.HOST,
-    user: 'root', // Myself.
-    password: '', // Levantar informacion sencible de Variables de Entorno (.ENV)
-    database: 'warehouse' // Nombre de la base de Datos
+require('dotenv').config();
+
+const DB = mysql.createConnection({ 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD, 
+    database:  process.env.DB_NAME 
 });
 
 // mover estos datos a la .env 
